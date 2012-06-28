ac4a095162d88f557aa0f15714e15f4afe2abdef
Matheus Paixao <mhepaixao@gmail.com>
2012-06-28 12:57:21 -0300

    hasPrecedent method implemented.

--- PrecedenceConstrainedAnt.java
+++ PrecedenceConstrainedAnt.java
@@ -5,4 +5,30 @@ public class PrecedenceConstrainedAnt extends Ant{
       super(antQ, q0, initialNode);
       this.precedencesMatrix = precedencesMatrix;
    }
+
+   public Node chooseNextNode(){
+      Node nextNode = super.chooseNextNode();
+
+      if(hasPrecedecessor(nextNode) == true){
+         addPredecessors(nextNode);
+      }
+
+      return nextNode;
+   }
+
+   private boolean hasPrecedecessor(Node requirement){
+      boolean result = false;
+
+      for(int i = 0; i <= precedencesMatrix[requirement.getIndex()].length - 1; i++){
+         if(precedencesMatrix[requirement.getIndex()][i] == 1){
+            result = true;
+            break;
+         }
+      }
+
+      return result;
+   }
+
+   private void addPredecessors(Node requirement){
+   }
 }
