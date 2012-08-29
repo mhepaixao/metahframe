02919b8285a37d9e69954e72f5cd706925eb3f64
Mathus Paixao <mhepaixao@gmail.com>
2012-08-29 13:29:24 -0300

    The TSP instance can be in cartesian coordinates or synmetric matrix format.

--- src/TSPInstanceReader.java
+++ src/TSPInstanceReader.java
@@ -43,12 +43,81 @@ public class TSPInstanceReader{
    /**
     * Method to get the distances matrix.
     *
+    * The instance can be in cartesian coordinates or synmetric matrix format.
+    * @author Matheus Paixao
+    * @return the distances matrix
+    * @see getInstanceFormat
+    * @see getDistancesMatrixInCartesianFormatInstance
+    * @see getDistancesMatrixInMatrixFormatInstance
+    */
+   public double[][] getDistancesMatrix(){
+      double[][] distancesMatrix = null;
+      String instanceType = getInstanceFormat();
+
+      if(instanceType.equals("coordinates")){
+         distancesMatrix = getDistancesMatrixInCartesianFormatInstance();
+      }
+      else if(instanceType.equals("matrix")){
+         distancesMatrix = getDistancesMatrixInMatrixFormatInstance();
+      }
+
+      return distancesMatrix;
+   }
+
+   /**
+    * Method to get the format of the instance.
+    *
+    * @author Matheus Paixao
+    * @return the format of the instance.
+    * @see regex package
+    * @see getInstance
+    */
+   private String getInstanceFormat(){
+      String instanceType = null;
+      String instanceLine = null;
+      String values[] = null;
+
+      try{
+         BufferedReader reader = new BufferedReader(new FileReader(getInstance()));
+         while(reader.ready()){
+            instanceLine = reader.readLine();
+
+            twoLettersMatcher = Pattern.compile("[a-z][a-z]").matcher(instanceLine) ;
+            numbersMatcher = Pattern.compile("[0-9]").matcher(instanceLine) ;
+
+            //if it isn't a text line and has numbers
+            if((twoLettersMatcher.find() == false) && (numbersMatcher.find() == true)){
+               spacesMatcher = Pattern.compile("\\s{2,}").matcher(instanceLine);
+               instanceLine = spacesMatcher.replaceAll(" ").trim(); //replace all spaces for just one
+
+               values = instanceLine.split(" ");
+               if(values.length > 3){
+                  instanceType = "matrix";
+                  break;
+               }
+               else{
+                  instanceType = "coordinates";
+                  break;
+               }
+            }
+         }
+      }
+      catch(Exception e){
+         System.out.println("Get instance format error");
+      }
+
+      return instanceType;
+   }
+
+   /**
+    * Method to get the distances matrix when the instance is in cartesian coordinates format.
+    *
     * @author Matheus Paixao
     * @return the distances matrix
     * @see getNodesList
     * @see calculateDistance
     */
-   public double[][] getDistancesMatrix(){
+   private double[][] getDistancesMatrixInCartesianFormatInstance(){
       Node[] nodes = getNodesList();
       double[][] distancesMatrix = new double[nodes.length][nodes.length];
 
@@ -201,5 +270,250 @@ public class TSPInstanceReader{
    private static double calculateDistance(Node node1, Node node2){
       return Math.sqrt(Math.pow(node1.getX() - node2.getX(), 2) + 
             Math.pow(node1.getY() - node2.getY(), 2));
+   }
+
+   /**
+    * Method to get the distances matrix when the instance is in matrix format.
+    *
+    * The matrix must be in one of the formats that follows:
+    * 1) Symmetric matrix, where the '0's delimits each row
+    *
+    * Let n be the number of nodes.
+    * 2) Symmetric matrix, where the first row is A1, A2 to A1, An. And the last row is An-1, An.
+    *
+    * @author Matheus Paixao
+    * @return the distances matrix
+    * @see getNumberOfNodesInMatrixFormat
+    */
+   public double[][] getDistancesMatrixInMatrixFormatInstance(){
+      double distancesMatrix[][] = null;
+
+      try{
+         String instanceLine = null;
+         String[] values;
+
+         BufferedReader reader = new BufferedReader(new FileReader(getInstance()));
+         while(reader.ready()){
+            instanceLine = reader.readLine();
+
+            Matcher twoLettersMatcher = Pattern.compile("[a-z][a-z]").matcher(instanceLine) ;
+            Matcher numbersMatcher = Pattern.compile("[0-9]").matcher(instanceLine) ;
+
+            //if it isn't a text line and has numbers
+            if((twoLettersMatcher.find() == false) && (numbersMatcher.find() == true)){
+               Matcher spacesMatcher = Pattern.compile("\\s{2,}").matcher(instanceLine);
+               instanceLine = spacesMatcher.replaceAll(" ").trim(); //replace all spaces for just one
+
+               values = instanceLine.split(" ");
+
+               //if the matrix is in format 1
+               if(Double.parseDouble(values[0]) == 0.0){
+                  distancesMatrix = getDistancesMatrixInMatrixFormat1();
+               }
+               else{
+                  distancesMatrix = getDistancesMatrixInMatrixFormat2();
+               }
+
+               //read only the first line of values
+               break;
+            }
+         }
+      }
+      catch(Exception e){
+         System.out.println("Error in get edges values matrix");
+         e.printStackTrace();
+      }
+
+      return distancesMatrix;
+   }
+
+   /**
+    * Method to get the distances matrix when the matrix instance is in format 1.
+    *
+    * Format 1: Symmetric matrix, where the '0's delimits each row.
+    * @author Matheus Paixao
+    * @return the distances matrix
+    * @see getNumberOfNodesInMatrixFormat1
+    */
+   private double[][] getDistancesMatrixInMatrixFormat1(){
+      int numberOfNodes = getNumberOfNodesInMatrixFormat1();
+      double distancesMatrix[][] = new double[numberOfNodes][numberOfNodes];
+
+      try{
+         String instanceLine = null;
+         String[] values;
+         int rowIndex = -1;
+         int columnIndex = -1;
+
+         BufferedReader reader = new BufferedReader(new FileReader(getInstance()));
+         while(reader.ready()){
+            instanceLine = reader.readLine();
+
+            Matcher twoLettersMatcher = Pattern.compile("[a-z][a-z]").matcher(instanceLine) ;
+            Matcher numbersMatcher = Pattern.compile("[0-9]").matcher(instanceLine) ;
+
+            //if it isn't a text line and has numbers
+            if((twoLettersMatcher.find() == false) && (numbersMatcher.find() == true)){
+               Matcher spacesMatcher = Pattern.compile("\\s{2,}").matcher(instanceLine);
+               instanceLine = spacesMatcher.replaceAll(" ").trim(); //replace all spaces for just one
+
+               values = instanceLine.split(" ");
+
+               //format 1
+               for(int i = 0; i <= values.length - 1; i++){
+                  //if it's a new row of the matrix
+                  if(Double.parseDouble(values[i]) == 0.0){
+                     rowIndex++;
+                     columnIndex = -1;
+                  }
+                  else{
+                     columnIndex++;
+                  }
+
+                  //symmetric matrix
+                  distancesMatrix[rowIndex][rowIndex + (columnIndex + 1)] = Double.parseDouble(values[i]);
+                  distancesMatrix[rowIndex + (columnIndex + 1)][rowIndex] = Double.parseDouble(values[i]);
+               }
+            }
+         }
+      }
+      catch(Exception e){
+         System.out.println("Error in get edges values matrix in format 1");
+         e.printStackTrace();
+      }
+
+      return distancesMatrix;
+   }
+
+   /**
+    * Method to get the number of nodes when the instance is in matrix format 1.
+    *
+    * Format 1: Symmetric matrix, where the '0's delimits each row.
+    * @author Matheus Paixao
+    * @return the number of nodes 
+    */
+   private int getNumberOfNodesInMatrixFormat1(){
+      int numberOfNodes = 0;
+
+      try{
+         String instanceLine = null;
+         String[] values;
+
+         BufferedReader reader = new BufferedReader(new FileReader(getInstance()));
+         while(reader.ready()){
+            instanceLine = reader.readLine();
+
+            Matcher twoLettersMatcher = Pattern.compile("[a-z][a-z]").matcher(instanceLine) ;
+            Matcher numbersMatcher = Pattern.compile("[0-9]").matcher(instanceLine) ;
+
+            //if it isn't a text line and has numbers
+            if((twoLettersMatcher.find() == false) && (numbersMatcher.find() == true)){
+               Matcher spacesMatcher = Pattern.compile("\\s{2,}").matcher(instanceLine);
+               instanceLine = spacesMatcher.replaceAll(" ").trim(); //replace all spaces for just one
+
+               values = instanceLine.split(" ");
+
+               for(int i = 0; i <= values.length - 1; i++){
+                  //if it's a new row of the matrix
+                  if(Double.parseDouble(values[i]) == 0.0){
+                     numberOfNodes++;
+                  }
+               }
+            }
+
+         }
+      }
+      catch(Exception e){
+         System.out.println("Error in get number od nodes in matrix format 1");
+         e.printStackTrace();
+      }
+
+      return numberOfNodes;
+   }
+
+   /**
+    * Method to get the distances matrix when the matrix is in format 2.
+    *
+    * Let n be the number of nodes.
+    * Format 2: Symmetric matrix, where the first row is A1, A2 to A1, An. And the last row is An-1, An.
+    * @author Matheus Paixao
+    * @return the distances matrix
+    * @see getNumberOfNodesInMatrixFormat2
+    */
+   private double[][] getDistancesMatrixInMatrixFormat2(){
+      int numberOfNodes = getNumberOfNodesInMatrixFormat2();
+      double distancesMatrix[][] = new double[numberOfNodes][numberOfNodes];
+
+      try{
+         String instanceLine = null;
+         String[] values;
+         int rowIndex = 0;
+
+         BufferedReader reader = new BufferedReader(new FileReader(getInstance()));
+         while(reader.ready()){
+            instanceLine = reader.readLine();
+
+            Matcher twoLettersMatcher = Pattern.compile("[a-z][a-z]").matcher(instanceLine) ;
+            Matcher numbersMatcher = Pattern.compile("[0-9]").matcher(instanceLine) ;
+
+            //if it isn't a text line and has numbers
+            if((twoLettersMatcher.find() == false) && (numbersMatcher.find() == true)){
+               Matcher spacesMatcher = Pattern.compile("\\s{2,}").matcher(instanceLine);
+               instanceLine = spacesMatcher.replaceAll(" ").trim(); //replace all spaces for just one
+
+               values = instanceLine.split(" ");
+
+               //format 2
+               for(int i = 0; i <= values.length - 1; i++){
+                  //symmetric matrix
+                  distancesMatrix[rowIndex][rowIndex + (i + 1)] = Double.parseDouble(values[i]);
+                  distancesMatrix[rowIndex + (i + 1)][rowIndex] = Double.parseDouble(values[i]);
+               }
+
+               rowIndex++;
+            }
+         }
+      }
+      catch(Exception e){
+         System.out.println("Error in get edges values matrix in format 2");
+         e.printStackTrace();
+      }
+
+      return distancesMatrix;
+   }
+
+   /**
+    * Method to get the number of nodes when the instance is in matrix format 2.
+    *
+    * Let n be the number of nodes.
+    * Format 2: Symmetric matrix, where the first row is A1, A2 to A1, An. And the last row is An-1, An.
+    * @author Matheus Paixao
+    * @return the number of nodes 
+    */
+   private int getNumberOfNodesInMatrixFormat2(){
+      int numberOfNodes = 0;
+
+      try{
+         String instanceLine = null;
+
+         BufferedReader reader = new BufferedReader(new FileReader(getInstance()));
+         while(reader.ready()){
+            instanceLine = reader.readLine();
+
+            Matcher twoLettersMatcher = Pattern.compile("[a-z][a-z]").matcher(instanceLine) ;
+            Matcher numbersMatcher = Pattern.compile("[0-9]").matcher(instanceLine) ;
+
+            //if it isn't a text line and has numbers
+            if((twoLettersMatcher.find() == false) && (numbersMatcher.find() == true)){
+               numberOfNodes++;
+            }
+         }
+      }
+      catch(Exception e){
+         System.out.println("Error in get number of nodes in matrix format");
+      }
+
+      //in symmetric matrix the last line is the node An-1
+      return numberOfNodes + 1;
    }
 }
