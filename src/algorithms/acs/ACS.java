c5ca4d00143d34399a5b3e94cc9b443d2b2f8183
Mathus Paixao <mhepaixao@gmail.com>
2012-10-03 10:02:50 -0300

    Constants and initNodes method implemented.

--- src/algorithms/acs/ACS.java
+++ src/algorithms/acs/ACS.java
@@ -2,9 +2,19 @@ package algorithms.acs;
 
 import algorithms.Algorithm;
 
-public class ACS implements Algorithm{
+public abstract class ACS implements Algorithm{
+   private static final double q0 = 0.9;
+   private static final double beta = 2;
+   private static final double alpha = 0.1;
+   private static final double rho = 0.1;
+
    private int numberOfIterations;
    private double totalTime;
+
+   private Integer[] nodes;
+   private double[][] pheromone;
+
+   protected abstract int getNumberOfNodes();
 
    public ACS(int numberOfIterations){
       setNumberOfIterations(numberOfIterations);
@@ -19,11 +29,37 @@ public class ACS implements Algorithm{
       this.totalTime = totalTime;
    }
 
+   public double getTotalTime(){
+      return this.totalTime;
+   }
+   
    public double getSolution(){
+      double initialTime = 0;
+      double finalTime = 0;
+
+      initACS();
+
+      initialTime = System.currentTimeMillis();
+      finalTime = System.currentTimeMillis();
+      setTotalTime(finalTime - initialTime);
+
       return 0;
    }
 
-   public double getTotalTime(){
-      return this.totalTime;
+   private void initACS(){
+      initNodes();
+      initPheromoneValues();
+   }
+
+   private void initNodes(){
+      nodes = new Integer[getNumberOfNodes()];
+
+      for(int i = 0; i <= nodes.length - 1; i++){
+         nodes[i] = i;
+      }
+   }
+
+   private void initPheromoneValues(){
+      //pheromone = new double
    }
 }
--- src/problems/tsp/TSPACS.java
+++ src/problems/tsp/TSPACS.java
@@ -1,12 +1,26 @@
 package problems.tsp;
 
 import algorithms.acs.ACS;
+import instancereaders.TSPInstanceReader;
 
 import java.io.File;
 
 public class TSPACS extends ACS{
+   private TSPInstanceReader tspInstanceReader;
+
+   private int numberOfCities;
+   private double[][] distancesMatrix;
+   private double initialPheromone;
 
    public TSPACS(File instance, int numberOfIterations){
       super(numberOfIterations);
+      this.tspInstanceReader = new TSPInstanceReader(instance);
+      this.distancesMatrix = tspInstanceReader.getDistancesMatrix();
+      this.numberOfCities = distancesMatrix.length; 
+      //this.initialPheromone = calculateInitialPheromone();
+   }
+
+   protected int getNumberOfNodes(){
+      return this.numberOfCities;
    }
 }
