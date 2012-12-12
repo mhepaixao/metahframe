77a63113dce79013dc2a1d2006dd1807e6b1a62c
Mathus Paixao <mhepaixao@gmail.com>
2012-12-12 17:05:08 -0300

    Init Simulate Annealing method implemented.

--- src/algorithms/sa/SimulatedAnnealing.java
+++ src/algorithms/sa/SimulatedAnnealing.java
@@ -5,13 +5,32 @@ import algorithms.Algorithm;
 import java.util.Random;
 
 public abstract class SimulatedAnnealing implements Algorithm{
+   private int numberOfIterations;
+   private double temperature;
+   private int numberOfMarkovChains;
+   private int[] bestSolutionSoFar;
+
+   protected abstract int[] getInitialSolution();
+   protected abstract int[] getNeighbourSolution(int[] solution);
+   protected abstract double calculateSolutionValue(int[] solution);
+
+   private Random random;
 
    public SimulatedAnnealing(int numberOfIterations){
-
+      this.numberOfIterations = numberOfIterations;
+      this.random = new Random();
    }
 
    public double getSolution(){
+      initSA();
+
       return 0;
+   }
+
+   private void initSA(){
+      temperature = 100;
+      numberOfMarkovChains = 10;
+      bestSolutionSoFar = getInitialSolution();
    }
 
    public double getTotalTime(){
--- src/problems/tsp/TSPSimulatedAnnealing.java
+++ src/problems/tsp/TSPSimulatedAnnealing.java
@@ -5,11 +5,47 @@ import instancereaders.TSPInstanceReader;
 
 import java.io.File;
 
+import java.util.Random;
+import java.util.ArrayList;
+
 public class TSPSimulatedAnnealing extends SimulatedAnnealing{
    private TSPProblem tspProblem;
 
    public TSPSimulatedAnnealing(TSPProblem tspProblem, int numberOfIterations){
       super(numberOfIterations);
       this.tspProblem = tspProblem;
+   }
+
+   protected int[] getInitialSolution(){
+      int[] initialSolution = new int[tspProblem.getNumberOfCities()];
+      Random random = new Random();
+      ArrayList<Integer> listToGetRandomCities = new ArrayList<Integer>();
+
+      for(int i = 0; i <= initialSolution.length - 1; i++){
+         listToGetRandomCities.add(i);
+      }
+
+      for(int i = 0; i <= initialSolution.length - 1; i++){
+         initialSolution[i] = getRandomCity(random, listToGetRandomCities);
+      }
+
+      return initialSolution;
+   }
+
+   private int getRandomCity(Random random, ArrayList<Integer> listToGetRandomCities){
+      int randomCity;
+      int randomIndex = random.nextInt(listToGetRandomCities.size());
+
+      randomCity = listToGetRandomCities.get(randomIndex);
+      listToGetRandomCities.remove(randomIndex);
+      return randomCity;
+   }
+
+   protected int[] getNeighbourSolution(int[] solution){
+      return null;
+   }
+
+   protected double calculateSolutionValue(int[] solution){
+      return 0;
    }
 }
