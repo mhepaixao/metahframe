db251e96ffe8bff79a7b1ffb6e7eb07ab1c1d63b
Mathus Paixao <mhepaixao@gmail.com>
2012-12-23 18:28:42 -0300

    Implementing the scenarios probabilities.

--- src/instancegenerators/RobustNRPInstanceGenerator.java
+++ src/instancegenerators/RobustNRPInstanceGenerator.java
@@ -1,5 +1,108 @@
-public class RobustNRPInstanceGenerator{
-   public static void main(String[] args){
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 
+import java.util.Random;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+public class RobustNRPInstanceGenerator{
+   private Random random;
+
+   private int numberOfRequirements;
+   private int numberOfScenarios;
+   private int requirementRange;
+
+   public RobustNRPInstanceGenerator(){
+      this.random = new Random();
+   }
+
+   private void getGenerationsParameters(String[] parameters){
+      numberOfRequirements = Integer.parseInt(parameters[0]);
+      numberOfScenarios = Integer.parseInt(parameters[1]);
+      requirementRange = Integer.parseInt(parameters[2]);
+   }
+
+   private void generateInstance(){
+      try{
+         BufferedWriter writer = new BufferedWriter(new FileWriter(new File("/home/mhepaixao/instancias/rnrp/instancia.txt")));
+
+         writer.write(numberOfRequirements + " " + numberOfScenarios);
+         writer.write("\n");
+         writer.write("\n");
+
+         for(int i = 0; i <= 9; i++){
+            writer.write(getScenariosProbabilities());
+            writer.write("\n");
+         }
+      
+         for(int i = 0; i <= numberOfScenarios - 1; i++){
+            writer.write(getRequirementsValues());
+            writer.write("\n");
+         }
+
+         writer.close();
+      }
+      catch(Exception e){
+         e.printStackTrace();
+      }
+   }
+
+   private String getRequirementsValues(){
+      String requirementsValues = null;
+
+      for(int i = 0; i <= numberOfRequirements - 1; i++){
+         if(requirementsValues == null){
+            requirementsValues = getRequirementValue() + "";
+         }
+         else{
+            requirementsValues += " " + getRequirementValue();
+         }
+      }
+
+      return requirementsValues;
+   }
+
+   private int getRequirementValue(){
+      return 1 + random.nextInt(requirementRange);
+   }
+
+   private String getScenariosProbabilities(){
+      String scenariosProbabilities = null;
+      double[] randomNumbers = new double[numberOfScenarios];
+      double randomNumbersSum = 0;
+
+      DecimalFormatSymbols dfs = new DecimalFormatSymbols();
+      dfs.setDecimalSeparator('.');
+      DecimalFormat df = new DecimalFormat();
+      df.setMaximumFractionDigits(1);
+      df.setMinimumFractionDigits(1);
+      df.setDecimalFormatSymbols(dfs);
+
+      scenariosProbabilities = null;
+
+      for(int i = 0; i <= randomNumbers.length - 1; i++){
+         randomNumbers[i] = random.nextDouble();
+         randomNumbersSum += randomNumbers[i];
+      }
+
+      for(int i = 0; i <= randomNumbers.length - 1; i++){
+         if(scenariosProbabilities == null){
+            scenariosProbabilities = df.format(randomNumbers[i] / randomNumbersSum) + "";
+         }
+         else{
+            scenariosProbabilities += " " + df.format(randomNumbers[i] / randomNumbersSum);
+         }
+      }
+
+      return scenariosProbabilities;
+   }
+
+   public static void main(String[] args){
+      RobustNRPInstanceGenerator instanceGenerator = new RobustNRPInstanceGenerator();
+
+      instanceGenerator.getGenerationsParameters(args);
+      instanceGenerator.generateInstance();
    }
 }
